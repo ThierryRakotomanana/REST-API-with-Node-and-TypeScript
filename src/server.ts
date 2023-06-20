c945214bd0f2bff5432e3e3950dbd2682951cb5f
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-06-20 20:31:51 +0300

    optimizing code

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-import express from 'express';
-import { Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express';
 
-const app = express();
+const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Application works!');
 });
 
 app.listen(3000, () => {
-  console.log('Application started on port 3000!');
+  console.log('Application started on port 3000! : http://localhost:3000');
 });
