21b1b80dbb7c2cf932a002fd8c0ffb19642f554c
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-06-20 20:35:01 +0300

    change the port to 8000

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express, { Express, Request, Response } from 'express';
-
+const port = 8000
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Application works!');
 });
 
-app.listen(3000, () => {
-  console.log('Application started on port 3000! : http://localhost:3000');
+app.listen(8000, () => {
+  console.log(`Application started on port ${port}! : http://localhost:8000`);
 });
